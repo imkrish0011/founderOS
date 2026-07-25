ee147173a8ba9d3ab024140f7dd96976bafa5f3d
Sanjay Sharma <ks9875277@gmail.com>
2026-07-25 21:15:24 +0530

    fix: replace broken youtube playlist with reliable Lofi Girl livestream

--- src/pages/Focus.tsx
+++ src/pages/Focus.tsx
@@ -304,8 +304,8 @@ export default function Focus() {
                 <iframe 
                   width="100%" 
                   height="100%" 
-                  src="https://www.youtube.com/embed/videoseries?list=PLp8f7jp0nePpk1LYE-lP50W2t13h0E4JQ&autoplay=1" 
-                  title="YouTube video player" 
+                  src="https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1" 
+                  title="Focus Ambience" 
                   frameBorder="0" 
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                   allowFullScreen
